ab4a48de4ff18d9d12381aaf062f08cc0c37a331
gabzzz-dias <gabriel.menezes.dias@hotmail.com>
2021-11-15 21:09:11 -0300

    pfv

--- models/chat.js
+++ models/chat.js
@@ -1,21 +1,14 @@
 const connection = require('./connection');
 
-const clearChat = async () =>
-  connection()
-  .then((db) => db.collection('messages')
-  .deleteMany({}));
+const clearChat = async () => connection().then((db) => db.collection('messages').deleteMany({}));
 
 const saveMessage = async (message) => {
-  connection()
-  .then((db) => db.collection('messages')
-  .insertOne(message));
+  connection().then((db) => db.collection('messages').insertOne(message));
 };
 
-const getMessages = async () =>
-  connection()
-  .then((db) => db.collection('messages')
-  .find()
-  .toArray());
+const getMessages = async () => {
+  connection().then((db) => db.collection('messages').find().toArray());
+};
 
 module.exports = {
   clearChat,
